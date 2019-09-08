44f44c781c02a07fcb65ef53c73502056e248c42
Ayush Jain <ayush@culturealley.com>
2019-09-09 03:27:27 +0530

    material template

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# [Portfolio](https://iamayushjain.github.io/)
-This is my portfolio which will guide you about the details you need to know about me such as my Projects, Resume, Contact details etc.
-
-# [Made with the help of](http://startbootstrap.com/) - [Freelancer](http://startbootstrap.com/template-overviews/freelancer/)
+# iamayushjain.github.io
---
+++ js/script.js
@@ -0,0 +1,117 @@
+$(function(){
+	'use strict';
+	
+	var portfolio = $('.portfolio-items'),
+		blog = $('.posts-grid');
+	
+	$('html').removeClass('no-js').addClass('js');
+	
+	/*=========================================================================
+		Skill Bars Initialization
+	=========================================================================*/
+	$('.skill').each(function(){
+		var $this = $(this),
+			percent = $this.data('percent') + '%';
+		$this.append("<div class='skill-bar' ><div class='percent' style='width:"+percent+"' ></div></div>");
+	});
+	
+	
+	/*=========================================================================
+		Magnific Popup (Project Popup initialization)
+	=========================================================================*/
+	$('.has-popup').magnificPopup({
+		type: 'inline',
+		fixedContentPos: false,
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
+	
+	$(window).on('load', function(){
+		
+		$('body').addClass('loaded');
+		
+		
+		var sect = window.location.hash;
+		if ( $(sect).length == 1 ){
+			$('.section.active').removeClass('active');
+			$(sect).addClass('active');
+		}
+		$(sect).focus();
+		
+		
+		/*=========================================================================
+			Portfolio Grid
+		=========================================================================*/
+		setTimeout(function(){
+			portfolio.shuffle();
+			blog.shuffle();
+		}, 1000);
+		
+		$('.portfolio-filters > li > a').on('click', function (e) {
+			e.preventDefault();
+			var groupName = $(this).attr('data-group');
+			$('.portfolio-filters > li > a').removeClass('active');
+			$(this).addClass('active');
+			portfolio.shuffle('shuffle', groupName );
+		});
+		
+		
+		/*=========================================================================
+			Testimonials Slider (Owl Carousel)
+		=========================================================================*/
+		$('.testimonials-slider').owlCarousel({
+			items: 1
+		});
+		
+	});
+	
+	
+	
+	$(window).on('resize', function(){
+		
+		
+		/*=========================================================================
+			Update the portfolio grid when window is resized
+		=========================================================================*/
+		setTimeout(function(){
+			portfolio.shuffle('update');
+			blog.shuffle('update');
+		},1000);
+		
+	});
+	
+	
+	/*=========================================================================
+		Menu Functions
+	=========================================================================*/
+	$('.menu-btn').on('click', function(e){
+		e.preventDefault();
+		$('body').toggleClass('show-menu');
+	});
+	
+	$('.menu-items > ul > li > a, .section-toggle').on('click', function(e){
+		
+		var $this = $(this),
+			section = $( '#' + $this.data('section') );
+		if( section.length != 0 ){
+			$('body').removeClass('show-menu');
+			$('.section.active').removeClass('active');
+			setTimeout(function(){
+				section.addClass('active');
+			}, 300);
+		}
+		
+		setTimeout(function(){
+			portfolio.shuffle();
+			blog.shuffle();
+		}, 1000);
+		
+	});
+	
+
+});
